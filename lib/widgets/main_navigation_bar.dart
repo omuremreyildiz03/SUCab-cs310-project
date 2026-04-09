d51c77becfb5af8ce8a514085d4ed86eed1644d9
Ahmet Hakan <ahmet.celikagi@sabanciuniv.edu>
2026-04-09 09:22:03 +0300

    Add files via upload

---
+++ lib/widgets/main_navigation_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+Widget MainNavigationBar(BuildContext context){
+
+  return BottomNavigationBar(
+    currentIndex: 0,
+    onTap: (index) {
+      switch (index) {
+        case 0:
+          Navigator.pushReplacementNamed(context, '/tickets');
+          break;
+        case 1:
+          Navigator.pushReplacementNamed(context, '/my_rides');
+          break;
+        case 2:
+          Navigator.pushReplacementNamed(context, '/profile');
+          break;
+      }
+    },
+    items: [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.airplane_ticket),
+        label: 'Tickets',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.car_rental),
+        label: 'My Rides',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: 'Profile',
+      ),
+    ],
+
+  );
+}
